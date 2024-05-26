8b1af722634278c0481d3db7c46ea71a773217f8
antoalex07 <antoalex710@gmail.com>
2024-05-26 23:53:17 +0530

    browser route set through an authorized path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import AuthWrapper from './auth/AuthWrapper';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <AuthWrapper/>
+      </BrowserRouter>
     </div>
   );
 }
