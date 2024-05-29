8d406f5f10621e38bd8ab095ed6e3fd2a37587ff
antoalex07 <antoalex710@gmail.com>
2024-05-29 23:51:39 +0530

    navigation path setup initiated

---
+++ src/components/structure/Navigation.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Home from '../pages/Home'
+
+const nav = [
+    { path: "/", name: "Home", element: <Home/>, isMenu: true, isPrivate: false }
+]
+
+export default nav
