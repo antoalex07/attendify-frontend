a77a6b6d0b0e5cfbcd0c0cfbe65872398bd9398b
antoalex07 <antoalex710@gmail.com>
2024-05-30 23:58:28 +0530

    home page created

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,41 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthData } from '../../auth/AuthWrapper';
 
 const Home = () => {
+  
+  const navigate = useNavigate();
+  const { user } = AuthData();
+
+  const handleButtonClick = () => {
+    navigate("/signUp");
+  }
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='primary-text'>
+        {user.isAuthenticated ? (
+          <h1>Welcome to workforce management with attendify</h1>
+        ) : (
+          <h1>Streamline your workforce management with attendify</h1>
+        )}
+        <div className='section-divider'/>
+        <p>Effortlessly manage employee tasks, attendance, and wages with our intuitive platform</p>
+        {!user.isAuthenticated ? (
+            <Button sx={{ fontWeight: 900, 
+                          width: "500px", 
+                          fontFamily: "Lato, sans seriff"}}
+                    variant='contained'
+                    onClick>
+          Sign Up</Button>    
+        ):(
+          <>
+          </>
+        )}
+      </div>
+      <div className='banner-image'></div>
+    </div>
   )
 }
 
